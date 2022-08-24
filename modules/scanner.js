3815867a7dd6c22670717f3e9fd1c2e3f244fab7
werlang <pswerlang@gmail.com>
2022-08-24 20:40:05 +0000

    Update save method to fill missing candles from data source.

--- modules/scanner.js
+++ modules/scanner.js
@@ -7,30 +7,6 @@ const scanner = {
     candles: {},
     resolution: 60, // 1 minute resolution
 
-    getMissingCandles: async function() {
-        const fromTime = this.getCandleId(this.config.fromTime);
-        const toTime = this.getCandleId(this.config.toTime);
-
-        const sql = `SELECT id, tsopen FROM candles WHERE id BETWEEN ? AND ?`;// AND id NOT BETWEEN 27349956 AND 27349965 AND id NOT BETWEEN 27570000 AND 27570150`;
-        const [ rows, error ] = await db.query(sql, [ fromTime, toTime ]);
-
-        // here we pick ranges present in database. ex: [1,10,12,23,56,99]
-        const ranges = rows.map(e => e.id).filter((e,i,a) => e+1 != a[i+1] || e-1 != a[i-1]);
-
-        // if the start of the range is not fetched
-        if (ranges[0] > fromTime) {
-            return fromTime;
-        }
-
-        // if the end of the range is not fetched
-        if (ranges[ ranges.length - 1 ] < toTime) {
-            return ranges[ ranges.length - 1 ];
-        }
-
-        // i=1 is the end of the first range. this should be the fromTime of the request
-        return ranges.length ? ranges[1] : fromTime;
-    },
-
     scan: async function() {
         const endpoint = 'klines';
 
@@ -62,15 +38,39 @@ const scanner = {
         return true;
     },
 
+    getMissingCandles: async function() {
+        const fromTime = this.getCandleId(this.config.fromTime);
+        const toTime = this.getCandleId(this.config.toTime);
+
+        const sql = `SELECT id, tsopen FROM candles WHERE id BETWEEN ? AND ?`;// AND id NOT BETWEEN 27349956 AND 27349965 AND id NOT BETWEEN 27570000 AND 27570150`;
+        const [ rows, error ] = await db.query(sql, [ fromTime, toTime ]);
+
+        // here we pick ranges present in database. ex: [1,10,12,23,56,99]
+        const ranges = rows.map(e => e.id).filter((e,i,a) => e+1 != a[i+1] || e-1 != a[i-1]);
+
+        // if the start of the range is not fetched
+        if (ranges[0] > fromTime) {
+            return fromTime;
+        }
+
+        // if the end of the range is not fetched
+        if (ranges[ ranges.length - 1 ] < toTime) {
+            return ranges[ ranges.length - 1 ];
+        }
+
+        // i=1 is the end of the first range. this should be the fromTime of the request
+        return ranges.length ? ranges[1] : fromTime;
+    },
+
     save: async function(data) {
         const fromStr = new Date(data[0][0]).toISOString();
         const toStr = new Date(data.slice(-1)[0][0]).toISOString();
         console.log(`Saving into database. FROM: ${ fromStr }, TO: ${ toStr }`);
 
-        const sql = `INSERT INTO candles (id, open, close, low, high, volume, tsopen, tsclose, samples) VALUES (:id,:open,:close,:low,:high,:volume,:tsOpen,:tsClose,:samples) ON DUPLICATE KEY UPDATE open = :open, close = :close, low = :low, high = :high, volume = :volume, tsopen = :tsOpen, tsclose = :tsClose, samples = :samples;`;
+        const sql = `INSERT INTO candles (id, open, close, low, high, volume, tsopen, tsclose, samples, missing) VALUES (:id,:open,:close,:low,:high,:volume,:tsOpen,:tsClose,:samples,:missing) ON DUPLICATE KEY UPDATE open = :open, close = :close, low = :low, high = :high, volume = :volume, tsopen = :tsOpen, tsclose = :tsClose, samples = :samples, missing = :missing;`;
         
-        const inserts = await Promise.all( data.map(async candle => {
-            const data = {
+        const inserts = await Promise.all( data.map(async (candle, i) => {
+            const newCandle = {
                 id: this.getCandleId(candle[0]),
                 tsOpen: new Date(candle[0]),
                 open: candle[1],
@@ -80,9 +80,35 @@ const scanner = {
                 volume: candle[5],
                 tsClose: new Date(candle[6]),
                 samples: candle[8],
+                missing: 0,
             };
 
-            return db.query(sql, data);
+            // this is a check to see if the data source is missing candles
+            if (i > 0) {
+                const diffId = this.getCandleId(candle[0]) - this.getCandleId(data[i-1][0]);
+                // if yes, then we create the missing candles from the previous one
+                if (diffId > 1) {
+                    const candlePrev = data[i-1];
+                    for (let j = 1 ; j < diffId ; j++) {
+                        const newCandle = {
+                            id: this.getCandleId(candlePrev[0]) + j,
+                            tsOpen: new Date( parseFloat(candlePrev[0]) + (j * this.resolution * 1000) ),
+                            tsClose: new Date( parseFloat(candlePrev[6]) + (j * this.resolution * 1000) ),
+                            open: candlePrev[1],
+                            high: candlePrev[2],
+                            low: candlePrev[3],
+                            close: candlePrev[4],
+                            volume: 0,
+                            samples: 0,
+                            missing: 1,
+                        };
+                        // console.log(newCandle)
+                        db.query(sql, newCandle);
+                    }
+                }
+            }
+
+            return db.query(sql, newCandle);
         }));
 
         inserts.forEach(([_,error]) => {
