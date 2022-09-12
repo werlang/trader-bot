e4011a330ac0f519db4996ef145b36050d49e62c
werlang <pswerlang@gmail.com>
2022-09-12 12:43:12 +0000

    Add getHistory to api module.

--- helper/api.js
+++ helper/api.js
@@ -7,20 +7,20 @@ const api = {
         const wallet = await api.wallet.get();
 
         if (currency && wallet.currency < amount) {
-            if (api.verbose > 0) {
+            if (config().verbose > 0) {
                 console.log('Not enough currency balance to perform trader swap.');
             }
             return false;
         }
 
         if (!currency && wallet.asset < amount) {
-            if (api.verbose > 0) {
+            if (config().verbose > 0) {
                 console.log('Not enough asset balance to perform this swap.');
             }
             return false;
         }
 
-        const swapFee = amount * api.swapFee;
+        const swapFee = amount * config().swapFee;
 
         if (currency) {
             await api.wallet.currency.add(-amount);
@@ -40,7 +40,7 @@ const api = {
             currency: currency,
         });
 
-        if (api.verbose > 1) {
+        if (config().verbose > 1) {
             const wallet = await api.wallet.get();
             let msg = api.candle.tsclose.toISOString() + '\t';
             msg += `SWAPPED: ${ amount.toFixed(8) } ${ currency ? 'C' : 'A' } @ ${ price }\t`;
@@ -59,18 +59,30 @@ const api = {
         const wallet = await api.wallet.get();
         return wallet.currency + wallet.asset * api.candle.close;
     },
+
+    getHistory: function(fromTime, toTime) {
+        if (!fromTime) return [];
+        if (!toTime) {
+            toTime = api.candle.tsclose;
+        }
+
+        fromTime = new Date(fromTime).getTime();
+        toTime = new Date(toTime).getTime();
+
+        return api.history.filter(candle => new Date(candle.tsopen).getTime() >= fromTime && new Date(candle.tsclose).getTime() <= toTime);
+    }
 }
 
 module.exports = (trader, strategy) => {
     strategy.getWallet = api.getWallet;
     strategy.getWalletBalance = api.getWalletBalance;
     strategy.swap = api.swap;
+    strategy.getHistory = api.getHistory;
     api.strategy = strategy;
 
     api.wallet = trader.wallet;
-    api.verbose = config().verbose;
-    api.swapFee = config().swapFee;
     api.traderReport = trader.report;
+    api.history = trader.data;
 
     return api;
 };
